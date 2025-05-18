bf02d31655ba3756f08a9e941fdea491ca06c894
AnthyOliveira <anthyoliveira@outlook.com>
2025-05-18 20:21:45 -0300

    .

--- nssm-service-example.bat
+++
@@ -1,5 +0,0 @@
-nssm install clarabot "C:\Program Files\nodejs\node.exe"
-nssm set clarabot AppDirectory "C:\clarabot"
-nssm set clarabot AppParameters index.js
-nssm set clarabot AppStdout "C:\clarabot\logs\clarabot.service.log"
-nssm set clarabot AppStderr "C:\clarabot\logs\clarabot.service.log"
